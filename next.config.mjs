76d76e82cfecbed968fd2c73cfaae5a425ef51f1
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2024-10-17 22:01:07 +1100

    updated preview players

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
